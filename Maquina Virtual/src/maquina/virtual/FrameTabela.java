ca803af86e8fe972e62ea7e297e4e8386759b902
murilopapa <murilopapa123@gmail.com>
2019-08-28 14:22:04 -0300

    arrumar tabelas

--- Maquina Virtual/src/maquina/virtual/FrameTabela.java
+++ Maquina Virtual/src/maquina/virtual/FrameTabela.java
@@ -5,32 +5,13 @@
  */
 package maquina.virtual;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Graphics;
-import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 import java.io.File;
-import javax.swing.ImageIcon;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
-/**
- *
- * @author matheus
- */
 public class FrameTabela extends JPanel {
 
     private FrameTabela frameTabela = this;
--- Maquina Virtual/src/maquina/virtual/MainFrame.java
+++ Maquina Virtual/src/maquina/virtual/MainFrame.java
@@ -1,20 +1,15 @@
 package maquina.virtual;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 import javax.swing.JPanel;
@@ -22,23 +17,22 @@ import javax.swing.JScrollPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class MainFrame extends JFrame {
+
     private MainFrame main_frame = this;
     // Tabela
-    private JTabbedPane painel;
+    //private JTabbedPane painel;
     // Menu superior
     private JMenuBar menuBar;
     private JMenu arquivos, executar, sobre;
     private JMenuItem importar;
     private Gerenciador INSTANCE = Gerenciador.getInstance();
-    private Object teste[][];
-    private JTable table;
-    
+    private Panel planoFundo = new Panel();
+
     public MainFrame() {
         super("Maquina Virtual");
-        
-        Panel planoFundo = new Panel();
+
         add(planoFundo);
-        
+
         // Variável Barra de Menu
         menuBar = new JMenuBar();
         // Variáveis Items Barra Menu
@@ -47,7 +41,7 @@ public class MainFrame extends JFrame {
         sobre = new JMenu("Sobre...");
         // Variáveis Opcoes Items Menu
         importar = new JMenuItem("Importar OBJ");
-        
+
         // Add Barra Menu
         setJMenuBar(menuBar);
         // Add Items Barra Menu
@@ -57,20 +51,23 @@ public class MainFrame extends JFrame {
         // Add Opcoes nos Items dos Menus
         // Arquivos
         arquivos.add(importar);
-        
+
         // Painel de instrucoes e plano de fundo
-        painel = new JTabbedPane();
+        //painel = new JTabbedPane();
         planoFundo.setLayout(new BorderLayout());
-        planoFundo.add(painel);
-        
+        //planoFundo.add(painel);
+
         MainFrame.TextFieldHandler handler = new MainFrame.TextFieldHandler();
         importar.addActionListener(handler);
 
     }
+
     public class Panel extends JPanel {
-         
+
     }
+
     private class TextFieldHandler implements ActionListener {
+
         public void actionPerformed(ActionEvent event) {
             if (event.getSource() == importar) {
                 JFileChooser abrir_arquivo = new JFileChooser();
@@ -79,22 +76,47 @@ public class MainFrame extends JFrame {
                 File arquivo_obj;
                 abrir_arquivo.setFileSelectionMode(JFileChooser.OPEN_DIALOG);
                 int valor = abrir_arquivo.showOpenDialog(null);
-                
+
                 if (valor == JFileChooser.APPROVE_OPTION) {
-                    arquivo_obj = abrir_arquivo.getSelectedFile();  //pego o arquivo escolhido
-                    FrameTabela frameTabela = new FrameTabela(arquivo_obj);
-                        
-        
-                  //  String [] colunas = {"Comando", "Instrução 1", "Instrução 2"};
-                  //  Object [][] dados = {
-                  //      {"Ana Monteiro", "48 9923-7898", "ana.monteiro@gmail.com"},
-                  //      {"João da Silva", "48 8890-3345", "joaosilva@hotmail.com"},
-                  //      {"Pedro Cascaes", "48 9870-5634", "pedrinho@gmail.com"}
-                  //  };
-                  //  JTable tabela = new JTable(dados, colunas);
-                  //  JScrollPane barraRolagem = new JScrollPane(tabela);
-                  //  barraRolagem.setBounds(25, 25, 950, 500);
-                  //  main_frame.add(barraRolagem);
+                    arquivo_obj = abrir_arquivo.getSelectedFile(); //pego o arquivo escolhido
+
+                    Object[][] codigo;
+                    String[] titulos;
+
+
+                    if (INSTANCE.LerArquivo(arquivo_obj)
+                            == false) {
+                        JOptionPane.showMessageDialog(null, "Erro ao abrir arquivo");
+                    }
+                    int TAM1 = (INSTANCE.TamanhoMemoria());    //quantidade de alunos
+                    int TAM2 = 5;
+
+                    codigo = new Object[TAM1][TAM2];
+                    titulos = new String[TAM2];
+
+                    titulos[0] = "LINHA";
+                    titulos[1] = "BREAKPOINT";
+                    titulos[2] = "COMANDO";
+                    titulos[3] = "ARG1";
+                    titulos[4] = "ARG2";
+                    Memoria recebe_memoria = INSTANCE.getMemoria();
+                    for (int i = 0;
+                            i < recebe_memoria.getTotalLinhas();
+                            i++) {
+                        Funcoes funcao_atual = recebe_memoria.getFuncByIndex(i);
+                        codigo[i][0] = i + 1;
+                        codigo[i][1] = "X";
+                        codigo[i][2] = funcao_atual.getFuncao();
+                        codigo[i][3] = funcao_atual.getArg1();
+                        codigo[i][4] = funcao_atual.getArg2();
+                    }
+                    JTable table = new JTable(codigo, titulos);
+
+                    JScrollPane scrollPane = new JScrollPane(table);
+                    table.setFillsViewportHeight(true);
+
+                    planoFundo.add(scrollPane);
+                    planoFundo.updateUI();
                 }
             }
         }
