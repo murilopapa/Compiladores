f9e6bdbf04b953494441c46aad469e4ca3bc0ec1
mzorzi <43408223+mzorzi@users.noreply.github.com>
2019-10-20 12:17:17 -0300

    Sintatico
    falta pegar o token e implementar as 2 chamadas

---
+++ Compilador/src/compilador/Sintatico.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compilador;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author math_
+ */
+public class Sintatico {
+    private Lexico lexico;
+    private Token token;
+    
+    AnalisadorSintatico(AnalisadorLexico analisadorLexico) throws IOException {
+        this.analisadorLexico = analisadorLexico;
+        analisaPrograma();
+    }
+}
